164d52bda425360a2d134f1502df77d2ccac68fe
AMUALLA <104879763+chryzanths@users.noreply.github.com>
2023-08-16 14:41:11 +0800

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello{
+
+  public static void main(String[] args){
+
+    System.out.println("Hello, Software Engineering 1!");
+    System.out.println("Student Name: AMUALLA, Chryzle A");
+  }
+}
