9e8f798dfeea309d9eae4171956846620888492d
Chryzle <chryzlamualla@gmail.com>
2023-08-24 21:12:32 +0800

    file added

--- Hello.java
+++ Hello.java
@@ -4,5 +4,8 @@ public class Hello{
 
     System.out.println("Hello, Software Engineering 1!");
     System.out.println("Student Name: AMUALLA, Chryzle A");
+
+    System.out.println ("Complicated world of SoftDev that is why it pays much!");
+
   }
 }
